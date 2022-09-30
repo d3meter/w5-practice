40245c30b6105c9f9d96c63817a2e1870a92843b
d3meter <mark.demeter3@gmail.com>
2022-09-30 12:22:45 +0200

    new branche third-version

--- frontend/script_button.js
+++ frontend/script_button.js
@@ -1,85 +1,85 @@
 const year = [
     {
-        "month": "January",
-        "id": "jan",
-        "nth": 1,
-        "days": 31,
+        month: "January",
+        id: "jan",
+        nth: 1,
+        days: 31,
     },
     {
-        "month": "February",
-        "id": "febr",
-        "nth": 2,
-        "days": 28,
+        month: "February",
+        id: "febr",
+        nth: 2,
+        days: 28,
     },
     {
-        "month": "March",
-        "id": "marc",
-        "nth": 3,
-        "days": 31,
+        month: "March",
+        id: "marc",
+        nth: 3,
+        days: 31,
     },
     {
-        "month": "April",
-        "id": "apr",
-        "nth": 4,
-        "days": 30,
+        month: "April",
+        id: "apr",
+        nth: 4,
+        days: 30,
     },
     {
-        "month": "May",
-        "id": "may",
-        "nth": 5,
-        "days": 31,
+        month: "May",
+        id: "may",
+        nth: 5,
+        days: 31,
     },
     {
-        "month": "June",
-        "id": "june",
-        "nth": 6,
-        "days": 30,
+        month: "June",
+        id: "june",
+        nth: 6,
+        days: 30,
     },
     {
-        "month": "July",
-        "id": "july",
-        "nth": 7,
-        "days": 31,
+        month: "July",
+        id: "july",
+        nth: 7,
+        days: 31,
     },
     {
-        "month": "August",
-        "id": "aug",
-        "nth": 8,
-        "days": 31,
+        month: "August",
+        id: "aug",
+        nth: 8,
+        days: 31,
     },
     {
-        "month": "September",
-        "id": "sept",
-        "nth": 9,
-        "days": 30,
+        month: "September",
+        id: "sept",
+        nth: 9,
+        days: 30,
     },
     {
-        "month": "October",
-        "id": "oct",
-        "nth": 10,
-        "days": 31,
+        month: "October",
+        id: "oct",
+        nth: 10,
+        days: 31,
     },
     {
-        "month": "November",
-        "id": "nov",
-        "nth": 11,
-        "days": 30,
+        month: "November",
+        id: "nov",
+        nth: 11,
+        days: 30,
     },
     {
-        "month": "December",
-        "id": "dec",
-        "nth": 12,
-        "days": 31,
-    }
+        month: "December",
+        id: "dec",
+        nth: 12,
+        days: 31,
+    },
 ];
 
 const monthComponent = function (nth, name, days) {
     let daysHTML = "";
-    
-    for (let i = 1; i <= days; i++){
+
+    for (let i = 1; i <= days; i++) {
         daysHTML += dayComponent(i);
     }
-    
+
     return `
         <section id="${nth}" class="${name}">    
             <h2>${name}</h2>
@@ -87,62 +87,50 @@ const monthComponent = function (nth, name, days) {
             <div class="selected-day"></div>
         
            </section>
-    `
-}
+    `;
+};
 
 const dayComponent = function (dayCount) {
     return `
         <div class="day">${dayCount}</div>
-    `
-}
+    `;
+};
 
 console.log("hello");
 const rootElement = document.querySelector("#root");
-/* console.log(year[0].days); */
 
 rootElement.insertAdjacentHTML("beforeend", "<button>Add month</button>");
 const buttonElement = rootElement.querySelector("button");
 
-/* let monthIndex = 0; */
-
-buttonElement.addEventListener("click", function() {
-/*     if (monthIndex<12){
-        rootElement.insertAdjacentHTML("beforeend", monthComponent(year[monthIndex].nth, year[monthIndex].month, year[monthIndex].days));
-        console.log(year[monthIndex]);
-        monthIndex++;
-    } else {
-        buttonElement.setAttribute("disabled","");
-    } */
-
+buttonElement.addEventListener("click", function () {
     for (let monthIndex = 0; monthIndex < 12; monthIndex++) {
-        rootElement.insertAdjacentHTML("beforeend", monthComponent(
-            year[monthIndex].nth,
-            year[monthIndex].month,
-            year[monthIndex].days));
-        }
-        initJanuaryEvents();
-    })
+        rootElement.insertAdjacentHTML(
+            "beforeend",
+            monthComponent(
+                year[monthIndex].nth,
+                year[monthIndex].month,
+                year[monthIndex].days
+            )
+        );
+    }
+
+    const dayElements = document.querySelectorAll(".day");
+    console.log(dayElements);
+
+    for (let i = 0; i < dayElements.length; i++) {
+        dayElements[i].addEventListener("click", function () {
+            dayElements[i].classList.toggle("clicked");
+        });
+    }
+});
 
 const hideDays = function (days) {
-    for(let day of days) {
+    for (let day of days) {
         day.classList.add("hidden");
     }
-}
+};
 
 const showDayInfo = function (dayIndex) {
     let selectedDay = document.querySelector(".January .selected-day");
-    selectedDay.textContent = `January ${dayIndex}`
-}
-    
-const initJanuaryEvents = function () {
-    let days = document.querySelectorAll(".January .days .day");
-    for(let day of days) {
-        day.addEventListener("click", function () {
-            hideDays(days);
-            showDayInfo(day.textContent);
-        })
-    }
-}
-
-/*     rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "January", 31));
-    rootElement.insertAdjacentHTML("beforeend", monthComponent(1, "February", 28)); */
+    selectedDay.textContent = `January ${dayIndex}`;
+};
